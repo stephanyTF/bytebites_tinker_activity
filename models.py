bc93197d53a8b02698aa9507e57867441f054ca5
stephanyTF <stephany.lam@codepath.org>
2026-09-25 11:04:12 -0700

    feat: Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,9 @@
+"""
+Summary of 4 Classes
+
+1. Customer - Has Name and Purchase History of Orders
+2. FoodItem - Has name, price, category, popularityRating
+3. Menu - Has a list of FoodItems and a method for filtering food by category
+4. Order - Has a list of selected FoodItems and a method for calculating the total price
+
+"""
