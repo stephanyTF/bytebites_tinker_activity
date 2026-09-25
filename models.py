370c63fec88c535cfea8f87d63b7a61f7f4dcd3d
stephanyTF <stephany.lam@codepath.org>
2026-09-25 12:07:31 -0700

    feat: Add initial scaffolds for ByteBites classes

--- models.py
+++ models.py
@@ -7,3 +7,38 @@ Summary of 4 Classes
 4. Order - Has a list of selected FoodItems and a method for calculating the total price
 
 """
+
+from typing import Optional
+
+
+class FoodItem:
+    def __init__(self, name: str, price: float, category: str, popularity_rating: float):
+        pass
+
+
+class Menu:
+    def __init__(self, items: Optional[list[FoodItem]] = None):
+        pass
+        
+
+    def filter_by_category(self, category: str) -> list[FoodItem]:
+        return []
+
+
+class Order:
+    def __init__(self, selected_items: Optional[list[FoodItem]] = None):
+
+    def add_item(self, item: FoodItem) -> None:
+        pass
+
+    def compute_total_cost(self) -> float:
+        return 0.0
+
+
+class Customer:
+    def __init__(self, name: str, purchase_history: Optional[list[Order]] = None):
+        pass
+      
+
+    def verify_user(self) -> bool:
+        return False
